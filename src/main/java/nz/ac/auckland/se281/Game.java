a4b74f070650d409a1f479353343c2422a9608bd
nicoa <18207@students.mrgs.school.nz>
2024-05-17 17:35:55 +1200

    Fixed the round variable to count correctly, Fixed the playerWin condition to the correct condition, changing when the player won

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -2,24 +2,23 @@ package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import nz.ac.auckland.se281.Main.Choice;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
-  private int gameRound = 1;
-  private String playerName = null; 
+  private int gameRound;
+  private String playerName = null;
   private String winner = null;
   private Difficulty difficultyChoice;
   private Choice playerChoice;
-  private List<Integer> playerActions = new ArrayList<>(); 
-  private List<Integer> playerWinHistory = new ArrayList<>(); 
+  private List<Integer> playerActions = new ArrayList<>();
+  private List<Integer> playerWinHistory = new ArrayList<>();
 
   /**
-   * This method Starts a Newgame 'Odds and Evens' With all the parameters 
-   * To start playing and adjust the options.
-   * 
+   * This method Starts a Newgame 'Odds and Evens' With all the parameters To start playing and
+   * adjust the options.
+   *
    * @param difficulty The chosen difficulty, changes AI behaviour
    * @param choice The player chooses if they are predicting odd or even
    * @param options Options[0] is the player name
@@ -29,31 +28,31 @@ public class Game {
     // Setting the private field variables
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
     playerName = options[0];
-    this.difficultyChoice = difficulty; 
-    this.playerChoice = choice; 
-    this.gameRound = 1; 
+    this.difficultyChoice = difficulty;
+    this.playerChoice = choice;
+    this.gameRound = 0;
 
     // Clear history
-    playerActions.clear(); 
-    playerWinHistory.clear(); 
+    playerActions.clear();
+    playerWinHistory.clear();
   }
 
   /**
-   * This method 'play' gets the initial user input while also
-   * Checking if it fits the allowable numbers. 0 and 5.
+   * This method 'play' gets the initial user input while also Checking if it fits the allowable
+   * numbers. 0 and 5.
    */
   public void play() {
-    String userInput; 
-    boolean inputPass = false; 
-    int integerUser; 
-    Choice numChoice; 
+    String userInput;
+    boolean inputPass = false;
+    int integerUser;
+    Choice numChoice;
 
     if (playerName == null) {
       MessageCli.GAME_NOT_STARTED.printMessage();
     } else {
       // Print with the round number then update the number of rounds
+      gameRound++;
       MessageCli.START_ROUND.printMessage(Integer.toString(gameRound));
-      gameRound++; 
 
       // Checking the user input
       do {
@@ -63,11 +62,11 @@ public class Game {
 
         // Checking if it is between 0 and 5
         if ((integerUser >= 0) && (integerUser <= 5)) {
-          inputPass = true; 
+          inputPass = true;
         } else {
           MessageCli.INVALID_INPUT.printMessage();
         }
-      } while(!inputPass);
+      } while (!inputPass);
       // Adding to the history of playerInputs
       playerActions.add(integerUser);
 
@@ -75,32 +74,33 @@ public class Game {
       MessageCli.PRINT_INFO_HAND.printMessage(playerName, userInput);
 
       // Getting the AI's fingers for the game
-      AI ai = AIFactory.createAi(difficultyChoice); 
-      int aiFingers = ai.play(gameRound, playerActions, playerChoice, playerWinHistory); 
+      AI ai = AIFactory.createAi(difficultyChoice);
+      int aiFingers = ai.play(gameRound, playerActions, playerChoice, playerWinHistory);
 
       MessageCli.PRINT_INFO_HAND.printMessage("HAL-9000", Integer.toString(aiFingers));
 
       // Checking who wins
       // Getting the sum
-      int roundSum = aiFingers + integerUser; 
+      int roundSum = aiFingers + integerUser;
       numChoice = getChoice(roundSum);
 
       // Changing the variable names for the print_outcome_round
-      String winner = playerName; 
+      String winner = playerName;
       if (numChoice != playerChoice) {
-        winner = "HAL-9000"; 
-        playerWinHistory.add(0); 
+        winner = "HAL-9000";
+        playerWinHistory.add(0);
       } else {
         playerWinHistory.add(1);
       }
-      
-      MessageCli.PRINT_OUTCOME_ROUND.printMessage(Integer.toString(roundSum), numChoice.toString(), winner);
+
+      MessageCli.PRINT_OUTCOME_ROUND.printMessage(
+          Integer.toString(roundSum), numChoice.toString(), winner);
     }
   }
 
   /**
-   * This method 'getChoice' finds the choice enum that the roundSum 
-   * equates to. 
+   * This method 'getChoice' finds the choice enum that the roundSum equates to.
+   *
    * @param roundSum The sum of the integers in the round
    * @return the choice ODD/EVEN
    */
@@ -108,7 +108,7 @@ public class Game {
     if (Utils.isEven(roundSum)) {
       return Choice.EVEN;
     } else {
-      return Choice.ODD; 
+      return Choice.ODD;
     }
   }
 
@@ -126,27 +126,29 @@ public class Game {
   }
 
   public void showStats() {
-    int playerRoundsWon = 0; 
+    int playerRoundsWon = 0;
     int playerRoundsLost = 0;
 
     if (playerName == null) {
       MessageCli.GAME_NOT_STARTED.printMessage();
     } else {
-      // Find out the playerRoundsWon 
-      playerRoundsWon = playerWinHistory.stream().mapToInt(Integer::intValue).sum(); 
-      playerRoundsLost = playerWinHistory.size() - playerRoundsWon; 
-      
+      // Find out the playerRoundsWon
+      playerRoundsWon = playerWinHistory.stream().mapToInt(Integer::intValue).sum();
+      playerRoundsLost = playerWinHistory.size() - playerRoundsWon;
+
       // Player Wins
-      MessageCli.PRINT_PLAYER_WINS.printMessage(playerName, Integer.toString(playerRoundsWon), Integer.toString(playerRoundsLost));
+      MessageCli.PRINT_PLAYER_WINS.printMessage(
+          playerName, Integer.toString(playerRoundsWon), Integer.toString(playerRoundsLost));
 
       // AI WIns
-      MessageCli.PRINT_PLAYER_WINS.printMessage("HAL-9000",Integer.toString(playerRoundsLost), Integer.toString(playerRoundsWon));
+      MessageCli.PRINT_PLAYER_WINS.printMessage(
+          "HAL-9000", Integer.toString(playerRoundsLost), Integer.toString(playerRoundsWon));
 
       // Setting who won
       if (playerRoundsWon > playerRoundsLost) {
-        winner = playerName; 
+        winner = playerName;
       } else if (playerRoundsWon < playerRoundsLost) {
-        winner = "HAL-9000"; 
+        winner = "HAL-9000";
       } else {
         winner = "Tie!";
       }
--- src/main/java/nz/ac/auckland/se281/HardAI.java
+++ src/main/java/nz/ac/auckland/se281/HardAI.java
@@ -1,56 +1,53 @@
 package nz.ac.auckland.se281;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import nz.ac.auckland.se281.Main.Choice;
 
+public class HardAI implements AI {
+  private Strategy strategy;
 
-public class HardAI implements AI{
-    private Strategy strategy; 
+  @Override
+  public Strategy getStrategy() {
+    return this.strategy;
+  }
 
-    @Override
-    public Strategy getStrategy() {
-        return this.strategy; 
-    }
-
-    @Override
-    public void setStrategy(Strategy strategyType) {
-        this.strategy = strategyType; 
-    }
+  @Override
+  public void setStrategy(Strategy strategyType) {
+    this.strategy = strategyType;
+  }
 
   /**
-     * Method 'play' Deals with the real time strategy changes 
-     * during the game, changing f
-     * 
-     * @param round The current round number in the game, dictates the strategy change
-     * @param playerAction The number the player chooses, needed for TopStrategy
-     * @return Returns the number generated from the strategy
-     */
-    @Override
-    public int play(int round, List<Integer> playerActions, Choice playerChoice, List<Integer> playerWinHistory) {
-        // Between rounds 1 and 3, use the random strategy
-        if (round <= 3) {
-            setStrategy(new RandomStrategy());
-        } else if (round > 3) {
-            setStrategy(new RandomStrategy());
-            // Checking the most recent win
-            if (playerWinHistory.get(playerWinHistory.size() - 1) == 0) {
-                // Switching the strategy if it doesnt work
-                if (strategy instanceof RandomStrategy) {
-                    setStrategy(new TopStrategy());
-                    ((TopStrategy) strategy).setPlayerChoice(playerChoice); 
-                    for (int action : playerActions) {
-                        ((TopStrategy) strategy).updateCounts(action);
-                    }
-                }
-            }
-        } else {
-            System.out.println("Error");
+   * Method 'play' Deals with the real time strategy changes during the game, changing f
+   *
+   * @param round The current round number in the game, dictates the strategy change
+   * @param playerAction The number the player chooses, needed for TopStrategy
+   * @return Returns the number generated from the strategy
+   */
+  @Override
+  public int play(
+      int round, List<Integer> playerActions, Choice playerChoice, List<Integer> playerWinHistory) {
+    // Between rounds 1 and 3, use the random strategy
+    if (round <= 3) {
+      setStrategy(new RandomStrategy());
+    } else if (round > 3) {
+      setStrategy(new RandomStrategy());
+      // Checking the most recent win
+      if (playerWinHistory.get(playerWinHistory.size() - 1) == 1) {
+        // Switching the strategy if it doesnt work
+        if (strategy instanceof RandomStrategy) {
+          setStrategy(new TopStrategy());
+          ((TopStrategy) strategy).setPlayerChoice(playerChoice);
+          for (int action : playerActions) {
+            ((TopStrategy) strategy).updateCounts(action);
+          }
         }
-        // Print the chosen strategy 
-        System.out.println("Chosen strategy: " + strategy.getClass().getSimpleName());
-        // Call the chosen strategy action
-        return strategy.getAction(); 
+      }
+    } else {
+      System.out.println("Error");
     }
+    // Print the chosen strategy
+    System.out.println("Chosen strategy: " + strategy.getClass().getSimpleName());
+    // Call the chosen strategy action
+    return strategy.getAction();
+  }
 }
